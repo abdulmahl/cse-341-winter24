618e310f5d437a8727229a339aa5d6a47ac3cb46
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-06 21:54:25 +0200

    Added Loui Route.

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,12 +1,17 @@
 const emilyRoute = (req, res) => {
-  res.send("Nodemon Dews");
+  res.send("Emily Dews");
+};
+
+const LouiRoute = (req, res) => {
+  res.send("Loui Dews");
 };
 
 const hannahRoute = (req, res) => {
-  res.send("Nodemon Dews");
+  res.send("Hannah Dews");
 };
 
 module.exports = {
     emilyRoute,
+    LouiRoute,
     hannahRoute
 } 
