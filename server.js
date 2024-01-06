35a55f6f313f933a2ac1f55a0bd14d7f278446a6
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-06 20:25:43 +0200

    Added lesson 1 ass...

---
+++ server.js
@@ -0,0 +1,11 @@
+// express web server
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Nodemon dew");
+});
+
+const port = 3000
+
+app.listen(process.env.port || port);
+console.log(`Web server is listening at port ${(process.env.port || port)}`);
