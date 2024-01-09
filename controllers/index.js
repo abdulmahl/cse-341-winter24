2a21aa1c75ad3271b58db3427a65a6906b82b78e
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-09 17:27:43 +0200

    Seperated ny routers

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const routeOne = (req, res) => {
+  res.send("Hello Route One / Home!");
+};
+
+const routeTwo = (req, res) => {
+  res.send("Hello Route Two!");
+};
+
+const routeThree = (req, res) => {
+  res.send("Hello Route Three!");
+};
+
+module.exports = { routeOne, routeTwo, routeThree };
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const week1Controller = require("./controllers/index");
+const week1Routes = require("./routes/index");
+
 const express = require("express"),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/", week1Controller.routeOne);
+app.get("/two", week1Controller.routeTwo);
+app.get("/three", week1Controller.routeThree);
 
 app.listen(port);
 console.log(`Listening at port: ${port}`);
