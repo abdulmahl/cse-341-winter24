39409d41f3515c8c44a3b11d74cebbbce4f4589e
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-09 18:50:12 +0200

    Removed sandbox folders and files.

--- route/routes.js
+++
@@ -1,9 +0,0 @@
-//? This is the routes controller
-const route = require("express").Router(); //* Router instance
-const controller = require("../controller/functions"); //* Import functions
-
-route.get("/", controller.routeOne);
-route.get("/two", controller.routeTwo);
-route.get("/three", controller.routeThree);
-
-module.exports = route;
