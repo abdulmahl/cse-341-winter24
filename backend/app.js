ab09760dada4141d036b3be50f8673c2ebae825b
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-10 12:04:39 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const profRouter = require("../backend/routes/prof");
+const professionalRoutes = require('./routes/prof');
+
 const app = express();
-
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Allow-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.use("/professional", profRouter);
-const port = 8080;
-app.listen(port);
-// console.log(`Listening on port ${port}`);
+app.use('/professional', professionalRoutes);
+
+app.listen(8080);
