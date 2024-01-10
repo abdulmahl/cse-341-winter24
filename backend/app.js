f71729ab617782bcf22df53cef6b8eaccbb8baa2
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-10 13:25:28 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use("/professional", professionalRoutes);
 
