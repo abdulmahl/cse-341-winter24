fbc4ec8622d2a07bfc3ea8bfe92db552a689f301
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-10 09:38:54 +0200

    .

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const professionalRoutes = require('./route/professional');
+
 const app = express();
 
-const port = process.env.PORT || 8080;
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-app.listen(port);
-console.log(`Web server is listening at port: ${port}`);
+app.use('/professional', professionalRoutes);
+
+app.listen(8080);
