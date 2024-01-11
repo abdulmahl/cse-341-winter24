1c63207d641d5961a361f6571cccc649f724f9d1
Abdul Mahl <mdu_abdul@icloud.com>
2024-01-11 14:47:08 +0200

    .

--- backend/routes/prof.js
+++ backend/routes/prof.js
@@ -4,6 +4,7 @@ const professionalController = require('../controllers/prof');
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get('/', professionalController.getData);
-
+// localhost:8080/professional/
 module.exports = router;
